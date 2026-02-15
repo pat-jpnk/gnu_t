65db7487ec69df7f291db6a176e741b2a2cdf352
pat-jpnk <jpnksoftware@protonmail.com>
2026-02-15 01:13:59 +0100

    saving

---
+++ lib/gcc/x86_64-pc-linux-gnu/13.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/home/jpnk/toolchains/.build/x86_64-pc-linux-gnu/src/gcc/configure --build=x86_64-build_pc-linux-gnu --host=x86_64-build_pc-linux-gnu --target=x86_64-pc-linux-gnu --prefix=/home/jpnk/x-tools/x86_64-pc-linux-gnu --exec_prefix=/home/jpnk/x-tools/x86_64-pc-linux-gnu --with-sysroot=/home/jpnk/x-tools/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sysroot --enable-languages=c,c++ --with-pkgversion='crosstool-NG 1.26.0' --enable-__cxa_atexit --disable-libmudflap --disable-libgomp --disable-libssp --disable-libquadmath --disable-libquadmath-support --disable-libsanitizer --enable-libmpx --with-gmp=/home/jpnk/toolchains/.build/x86_64-pc-linux-gnu/buildtools --with-mpfr=/home/jpnk/toolchains/.build/x86_64-pc-linux-gnu/buildtools --with-mpc=/home/jpnk/toolchains/.build/x86_64-pc-linux-gnu/buildtools --with-isl=/home/jpnk/toolchains/.build/x86_64-pc-linux-gnu/buildtools --enable-lto --enable-threads=posix --enable-target-optspace --enable-plugin --enable-gold --disable-nls --disable-multilib --with-local-prefix=/home/jpnk/x-tools/x86_64-pc-linux-gnu/x86_64-pc-linux-gnu/sysroot --enable-long-long";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
